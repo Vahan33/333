2dcd6cf56007a11851a0c8b7e0db4b562985488d
_vahan33__ <sargsyanvahanch33@gmail.com>
2024-09-21 11:32:16 +0400

    first commit

---
+++ 333_Homework.py
@@ -0,0 +1,32 @@
+# EX_1
+
+# age = int(input("age="))
+# if age < 18:
+#     print("you are under 18")
+# else:
+#     print("you are over 18")
+# if age < 0:
+#     print("You were not born yet")
+
+# EX_2
+
+# a = float(input("a="))
+# b = float(input("b="))
+# if a * b % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# EX_3
+
+# x = float(input("x="))
+# y = float(input("y="))
+# if x > y:
+#     print(x)
+# else:
+#     print(y)
+
+
+
+print(1, 2, 7, 84, 1045, 333, sep='', end='\n')
+print('hello')
---
+++ Homework.py
@@ -0,0 +1,58 @@
+# EX_1
+# x = float(input("x ="))
+# y = float(input("y ="))
+# print(x + y)
+
+# x = float(input("x ="))
+# y = float(input("y ="))
+# print(x - y)
+
+# x = float(input("x ="))
+# y = float(input("y ="))
+# print(x * y)
+
+# x = float(input("x ="))
+# y = float(input("y ="))
+# print(x / y)
+
+# x = float(input("x ="))
+# y = float(input("y ="))
+# print(x // y)
+
+
+# x = float(input("x ="))
+# y = float(input("y ="))
+# print(x % y)
+
+# x = int(input("x="))
+# y = int(input("y="))
+# print(x ** y)
+
+# x = float(input("x="))
+# print(x ** 0.5)
+
+# x = int(input("x="))
+# print(abs(x))
+
+# x = float(input("x="))
+# y = float(input("y="))
+# print(round(x / y))
+
+
+
+
+
+# EX_2
+# name = input("name= ")
+# print(name)
+
+# Surname = input("Surname=")
+# print(Surname)
+
+# Age = input("How old are you ?")
+# print(Age)
+
+
+# Info_1 = input("info_1=")
+# Info_2 = input("info_2=")
+# print("Hello",Info_1,"." "You are", Info_2, "years old."  )
